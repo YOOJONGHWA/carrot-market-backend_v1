0411f47cd4b6416f3c7593a6b6930e188d7fcb61
유종화 <sola201@naver.com>
2024-08-02 23:45:30 +0900

    Initial commit

---
+++ src/main/java/study/carrotmarketbackend_v1/CarrotMarketBackendV1Application.java
@@ -0,0 +1,13 @@
+package study.carrotmarketbackend_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMarketBackendV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotMarketBackendV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/study/carrotmarketbackend_v1/CarrotMarketBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.carrotmarketbackend_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotMarketBackendV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
